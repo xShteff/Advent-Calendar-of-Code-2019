8fae59f425bd7eac2cba4a8d85e8dc5879f999b9
Alin Stefan Olaru <alin.stefan.olaru@lego.com>
2019-12-02 13:06:50 +0100

    part 2

--- day1/index.js
+++ day1/index.js
@@ -1,12 +1,30 @@
-var inputData = require('./input');
+var inputData = require("./input");
 
 function calculateFuel(val) {
-    return Math.floor(val / 3 - 2);
+  return Math.floor(val / 3 - 2);
 }
 
-let result = inputData.reduce((a, b) => {
+let totalFuel =
+  inputData.reduce((a, b) => {
     return a + calculateFuel(b);
-}) - inputData[0] + calculateFuel(inputData[0]); // Botching this in for now
+  }) -
+  inputData[0] +
+  calculateFuel(inputData[0]); // Botching this in for now
 
-console.log(`The result is: ${result}`);
+console.log(`The amount of fuel required is: ${totalFuel}`);
 
+let p2Data = inputData.map((val, index) => {
+  let fuelIncrement = calculateFuel(val);
+  let fuelSum = fuelIncrement;
+  while (fuelIncrement > 0) {
+    fuelIncrement = calculateFuel(fuelIncrement);
+    if (fuelIncrement > 0) {
+      fuelSum += fuelIncrement;
+    }
+  }
+  return fuelSum;
+});
+
+console.log(
+  `Total amount fuel required (p2): ${p2Data.reduce((a, b) => a + b)}`
+);
