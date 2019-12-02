d6a1cabf495bc181528cdee3f441cb47e6f6f0f6
Alin Stefan Olaru <alin.stefan.olaru@lego.com>
2019-12-02 14:39:37 +0100

    I dont get this

--- day2/index.js
+++ day2/index.js
@@ -1,3 +1,63 @@
 const inputData = require('./input');
 
-console.log(inputData);
+function printIntcodeFancy(data) {
+  let index = 0;
+  console.log('[');
+  while (index < data.length) {
+    let line = '\t';
+    for (let x = 0; x < 4; x++) {
+      if (data[index] === 99) {
+        x = 4;
+      }
+      line += (data[index] !== undefined ? data[index] : '') + '\t';
+      index++;
+    }
+    console.log(line);
+  }
+  console.log(']');
+}
+
+function printIntcodeInline(data) {
+  let line = '';
+  for (let i = 0; i < data.length; i++) {
+    line += `${data[i]}, `;
+  }
+  console.log(line);
+}
+
+function handleIntcode(data, index) {
+  switch (data[index]) {
+    case 1:
+      data[data[index + 3]] = data[data[index + 1]] + data[data[index + 2]];
+      break;
+    case 2:
+      data[data[index + 3]] = data[data[index + 1]] * data[data[index + 2]];
+      break;
+    case 99:
+      return 0;
+    default:
+      return -1;
+  }
+  return 1;
+}
+// let inputData = [1, 1, 1, 4, 99, 5, 6, 0, 99];
+console.log('BEFORE: ');
+printIntcodeFancy(inputData);
+printIntcodeInline(inputData);
+let index = 0;
+
+while (index < inputData.length - 4) {
+  switch (handleIntcode(inputData, index)) {
+    case 1:
+      index += 4;
+      break;
+    case 0:
+      index += 1;
+      break;
+  }
+}
+console.log('AFTER: ');
+
+printIntcodeFancy(inputData);
+printIntcodeInline(inputData);
+console.log(inputData[0]);
