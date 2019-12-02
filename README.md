a3f64b712ae8348f8b5642f6d8ebcd9d42e8d722
Alin Stefan Olaru <alin.stefan.olaru@lego.com>
2019-12-02 13:19:52 +0100

    Ok now we're doing proper node

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Advent-Calendar-of-Code-2019
+
+I guess I'm joining the Advent Calendar thingy? I'm writing everything with Node.js. To run the project simply run `npm start` in your console.
--- day1/index.js
+++ day1/index.js
@@ -1,28 +1,30 @@
-const inputData = require("./input");
+module.exports = () => {
+  const inputData = require("./input");
 
-function calculateFuel(val) {
-  return Math.floor(val / 3 - 2);
-}
-
-const totalFuel =
-  inputData.reduce((a, b) => {
-    return a + calculateFuel(b);
-  }) -
-  inputData[0] +
-  calculateFuel(inputData[0]); // Botching this in for now
-
-console.log(`The amount of fuel required is: ${totalFuel}`);
-
-const p2Data = inputData.map((val, index) => {
-  let fuelIncrement = calculateFuel(val);
-  let fuelSum = 0;
-  while (fuelIncrement > 0) {
-    fuelSum += fuelIncrement;
-    fuelIncrement = calculateFuel(fuelIncrement);
+  function calculateFuel(val) {
+    return Math.floor(val / 3 - 2);
   }
-  return fuelSum;
-});
 
-console.log(
-  `Total amount fuel required (p2): ${p2Data.reduce((a, b) => a + b)}`
-);
+  const totalFuel =
+    inputData.reduce((a, b) => {
+      return a + calculateFuel(b);
+    }) -
+    inputData[0] +
+    calculateFuel(inputData[0]); // Botching this in for now
+
+  console.log(`The amount of fuel required is: ${totalFuel}`);
+
+  const p2Data = inputData.map((val, index) => {
+    let fuelIncrement = calculateFuel(val);
+    let fuelSum = 0;
+    while (fuelIncrement > 0) {
+      fuelSum += fuelIncrement;
+      fuelIncrement = calculateFuel(fuelIncrement);
+    }
+
+    return fuelSum;
+  });
+  console.log(
+    `Total amount fuel required (p2): ${p2Data.reduce((a, b) => a + b)}`
+  );
+};
